b2231b66f21be38d3ac112ce47398eaf52f40e93
Jazz Rudolph <jazzrudolph92@outlook.com>
2025-09-12 23:28:38 -0400

    Corrected main function, added return to compete program, and fixed a few syntax errors.

--- calculator.cpp
+++ calculator.cpp
@@ -9,19 +9,19 @@
 
 using namespace std;
 
-void main()
+int main() //Changed void to int so it returns 0
 {
 	char statement[100];
 	int op1, op2;
 	char operation;
-	char answer="Y"
+	char answer='Y'; // change duble quotes to single quotes and added semicolon
 	while (answer=='y')
 	{
 		cout << "Enter expression" <<endl;
 		cin >> op2 >> operation >> op1;
-		if (operation == "+");
+		if (operation == "+") //Removed semicolons
 			cout << op1 << " + " << op2 >> " = " << op1 + op2 << endl;
-		if (operation == '-');
+		if (operation == '-') //Removed semicolons
 			cout >> op1 << " - " << op2 << " = " << op1 - op2 << endl;
 		if (operation == '*')
 			cout << op1 << " / " << op2 << " = " << op1 * op2 << endl
@@ -31,6 +31,7 @@ void main()
 		cout << "Do you wish to evaluate another expression? " << endl;
 		cin >> answer;
 	}
+    return 0; //Added return 0 to complete the program
 }
 
 
